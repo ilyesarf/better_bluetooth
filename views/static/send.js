fc6ebcf2b3e12197f72c30e83f686f36eab64340
Quimzy <bam.boy2020@gmail.com>
2023-01-24 17:01:13 +0300

    no send button

--- views/static/send.js
+++ views/static/send.js
@@ -1,21 +1,26 @@
 send_file = function(){
 	
 	let name = document.cookie.split('name=')[1]
-	let recv_id = document.getElementById('recv_id').value
-	let file = document.getElementById('file_content').files[0]
-
 	const peer = new Peer(name, {host: location.hostname, port: 9000, key: 'peerjs', path: '/peerjs'})
+	
 
-	const conn = peer.connect(recv_id + '-recv')
-	
-	conn.on('open', () =>{
-		const blob = new Blob(document.getElementById('file_content').files, {filetype: file.type})
-	
-		conn.send({
-			content: blob,
-			filename: file.name,
-			filetype: file.type
+	document.getElementById('file_content').onchange = () => {
+		
+		let recv_id = document.getElementById('recv_id').value
+		let file = document.getElementById('file_content').files[0]
+		
+		const conn = peer.connect(recv_id + '-recv')
+		
+		conn.on('open', () =>{
+			const blob = new Blob(document.getElementById('file_content').files, {filetype: file.type})
+		
+			conn.send({
+				content: blob,
+				filename: file.name,
+				filetype: file.type
+			})
 		})
-	})	
-	
+	}
+
+
 }
